430895f27a15f957874dc86f547f5a06c8d3c5a0
agilalatas <agilalatas17@gmail.com>
2025-06-19 02:11:08 +0700

    konfigurasi react client-side inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    @vite('resources/css/app.css')
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     {{-- Inertia --}}
     @inertiaHead
 </head>

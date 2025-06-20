e1ccc24128b7e54714e7c38129ac4926288eb8ad
agilalatas <agilalatas17@gmail.com>
2025-06-20 12:33:29 +0700

    membuat tampilan halaman transaksi

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function () {
+    return redirect('/food');
+});
+
 Route::get('/food', [FoodController::class, 'index'])->name('food.index');
 Route::inertia('/food/create', 'Food/Create')->name('food.create');
 
-// Route::get('/food', [FoodController::class, 'index'])->name('food');
-// Route::inertia('/create', 'create-food')->name('create-food');
-
 Route::get('/transaksi', function () {
-    return Inertia::render('transaksi');
+    return Inertia::render('Transaksi');
 })->name('transaksi');

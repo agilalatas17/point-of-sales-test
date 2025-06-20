b4de1551254671b67447a8721c82e4d4e2113d4c
agilalatas <agilalatas17@gmail.com>
2025-06-20 13:44:13 +0700

    Rapihin tampilan halaman transaksi dan ready to integrasi database

--- resources/js/pages/Transaksi.jsx
+++ resources/js/pages/Transaksi.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Space, Button } from "antd";
 
 export default function TransaksiPage() {
     const { Meta } = Card;
@@ -13,14 +13,14 @@ export default function TransaksiPage() {
                             cover={
                                 <img
                                     alt="example"
-                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
                                     height={228}
                                 />
                             }
                         >
                             <Meta
-                                title="Europe Street beat"
-                                description="www.instagram.com"
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
                                 className="text-center"
                             />
                         </Card>
@@ -30,14 +30,14 @@ export default function TransaksiPage() {
                             cover={
                                 <img
                                     alt="example"
-                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
                                     height={228}
                                 />
                             }
                         >
                             <Meta
-                                title="Europe Street beat"
-                                description="www.instagram.com"
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
                                 className="text-center"
                             />
                         </Card>
@@ -47,14 +47,14 @@ export default function TransaksiPage() {
                             cover={
                                 <img
                                     alt="example"
-                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
                                     height={228}
                                 />
                             }
                         >
                             <Meta
-                                title="Europe Street beat"
-                                description="www.instagram.com"
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
                                 className="text-center"
                             />
                         </Card>
@@ -64,14 +64,14 @@ export default function TransaksiPage() {
                             cover={
                                 <img
                                     alt="example"
-                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
                                     height={228}
                                 />
                             }
                         >
                             <Meta
-                                title="Europe Street beat"
-                                description="www.instagram.com"
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
                                 className="text-center"
                             />
                         </Card>
@@ -81,26 +81,190 @@ export default function TransaksiPage() {
                             cover={
                                 <img
                                     alt="example"
-                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
                                     height={228}
                                 />
                             }
                         >
                             <Meta
-                                title="Europe Street beat"
-                                description="www.instagram.com"
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
+                                className="text-center"
+                            />
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{ width: 320 }}
+                            cover={
+                                <img
+                                    alt="example"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
+                                    height={228}
+                                />
+                            }
+                        >
+                            <Meta
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
+                                className="text-center"
+                            />
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{ width: 320 }}
+                            cover={
+                                <img
+                                    alt="example"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
+                                    height={228}
+                                />
+                            }
+                        >
+                            <Meta
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
+                                className="text-center"
+                            />
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{ width: 320 }}
+                            cover={
+                                <img
+                                    alt="example"
+                                    src="https://www.finnafood.com/blog/wp-content/uploads/2024/04/resep-tahu-gejrot.webp"
+                                    height={228}
+                                />
+                            }
+                        >
+                            <Meta
+                                title="Tahu Gejrot"
+                                description="Rp 30.000"
                                 className="text-center"
                             />
                         </Card>
                     </Row>
                 </Col>
-                <Col span={8}>
-                    <Card>
-                        <h2 className="text-center">Pesanan</h2>
+                <Col span={8} className="p-0">
+                    <Card className="w-full">
+                        <h2 className="text-center mb-8">Pesanan</h2>
 
                         <main>
-                            <img src="" alt="" />
+                            <section className="flex items-center gap-3 justify-between mb-2">
+                                <Space size="middle">
+                                    <img
+                                        src="https://cdn-1.timesmedia.co.id/images/2023/06/03/Nasi-Jamblang-3.jpg"
+                                        width={140}
+                                        height={80}
+                                    />
+                                    <p className="truncate w-56 max-w-60">
+                                        Nasi Jamblang
+                                    </p>
+                                </Space>
+                                <Space size={26}>
+                                    <p>x4</p>
+                                    <p>Rp 39999</p>
+                                </Space>
+                            </section>
+                            <section className="flex items-center gap-3 justify-between mb-2">
+                                <Space size="middle">
+                                    <img
+                                        src="https://cdn-1.timesmedia.co.id/images/2023/06/03/Nasi-Jamblang-3.jpg"
+                                        width={140}
+                                        height={80}
+                                    />
+                                    <p className="truncate w-56 max-w-60">
+                                        Sate ayam ayam ayamdsadasdsadasdsad
+                                    </p>
+                                </Space>
+                                <Space size={26}>
+                                    <p>x4</p>
+                                    <p>Rp 39999</p>
+                                </Space>
+                            </section>
+                            <section className="flex items-center gap-3 justify-between mb-2">
+                                <Space size="middle">
+                                    <img
+                                        src="https://cdn-1.timesmedia.co.id/images/2023/06/03/Nasi-Jamblang-3.jpg"
+                                        width={140}
+                                        height={80}
+                                    />
+                                    <p className="truncate w-56 max-w-60">
+                                        Sate ayam ayam ayamdsadasdsadasdsad
+                                    </p>
+                                </Space>
+                                <Space size={26}>
+                                    <p>x4</p>
+                                    <p>Rp 39999</p>
+                                </Space>
+                            </section>
+                            <section className="flex items-center gap-3 justify-between mb-2">
+                                <Space size="middle">
+                                    <img
+                                        src="https://cdn-1.timesmedia.co.id/images/2023/06/03/Nasi-Jamblang-3.jpg"
+                                        width={140}
+                                        height={80}
+                                    />
+                                    <p className="truncate w-56 max-w-60">
+                                        Sate ayam ayam ayamdsadasdsadasdsad
+                                    </p>
+                                </Space>
+                                <Space size={26}>
+                                    <p>x4</p>
+                                    <p>Rp 39999</p>
+                                </Space>
+                            </section>
+                            <section className="flex items-center gap-3 justify-between mb-2">
+                                <Space size="middle">
+                                    <img
+                                        src="https://cdn-1.timesmedia.co.id/images/2023/06/03/Nasi-Jamblang-3.jpg"
+                                        width={140}
+                                        height={80}
+                                    />
+                                    <p className="truncate w-56 max-w-60">
+                                        Sate ayam ayam ayamdsadasdsadasdsad
+                                    </p>
+                                </Space>
+                                <Space size={26}>
+                                    <p>x4</p>
+                                    <p>Rp 39999</p>
+                                </Space>
+                            </section>
                         </main>
+
+                        <div className="mt-12 mb-2">
+                            <Button
+                                color="danger"
+                                variant="outlined"
+                                size="large"
+                                className="w-full"
+                            >
+                                Clear Cart
+                            </Button>
+                            <Row wrap={false} className="gap-4 my-4">
+                                <Button
+                                    variant="solid"
+                                    className="bg-green-500 text-white w-full"
+                                    size="large"
+                                >
+                                    Save Bill
+                                </Button>
+                                <Button
+                                    variant="solid"
+                                    className="bg-green-500 text-white w-full"
+                                    size="large"
+                                >
+                                    Print Bill
+                                </Button>
+                            </Row>
+                            <Button
+                                variant="solid"
+                                size="large"
+                                className="bg-sky-400 text-white w-full"
+                            >
+                                Charge Rp 40.000
+                            </Button>
+                        </div>
                     </Card>
                 </Col>
             </Row>
